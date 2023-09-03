2774f43857cc283fef30dd7051173488d1ee305e
kaoiki <zhouc@kaoiki.com>
2023-09-03 21:13:38 +0800

    edit basis.ts

--- basis.ts
+++ basis.ts
@@ -1,8 +1,26 @@
+import { Play } from "@icon-park/vue-next";
+
 class Girl {
     name(): void{
         console.log("I'm a girl");
     }
 }
 
-var obj = new Girl();
-obj.name();
+class Player {
+    name(): void{
+        console.log("I am Ka01Ki")
+    }
+}
+
+// var obj = new Girl();
+// obj.name();
+var play = new Player();
+play.name();
+
+let name: string = "Runoob";
+let years: number = 5;
+let words: string = `您好，今年是 ${ name } 发布 ${ years + 1} 周年`;
+console.log(words);
+
+let arrayList: any[] = [1, false, 'fine']
+console.log(arrayList[2])
