ab190ed60a5f8a7e000c4afc031eea1aa3645d0e
kaoiki <zhouc@kaoiki.com>
2023-09-10 21:45:12 +0800

    add ts_api.ts

--- ts_api.ts
+++ ts_api.ts
@@ -5,11 +5,12 @@ interface Employee {
 }
 
 let employee: Employee = {
-    firstName : "Emil",
-    lastName: "Andersson",
+    firstName : "Zhou",
+    lastName: "daodao",
     fullName(): string {
         return this.firstName + " " + this.lastName;
     }
 }
+
 
 console.log(employee.fullName());
