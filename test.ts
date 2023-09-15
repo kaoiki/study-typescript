3d3d394120e5501f1f231584c9cddc5d1c2234c2
kaoiki <zhouc@kaoiki.com>
2023-09-16 03:53:15 +0800

    update test.ts

--- test.ts
+++ test.ts
@@ -2,3 +2,6 @@ let myadd = function(a:number, b:number):number {
     return a+b;
 }
 console.log("5+4 = "+myadd(5,4))
+
+let youradd = (a:number, b:number):number =>{return a+b}
+console.log("6+7=",youradd(6,7)) 
